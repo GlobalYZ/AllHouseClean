caddf62dff077a3a35e3d2cb01041594d1a1636b
Muyang Li <globalyy2020@gmail.com>
2025-02-28 12:01:35 -0700

    add custom scroll bar style

--- src/components/About/CardHeader.tsx
+++ src/components/About/CardHeader.tsx
@@ -15,7 +15,9 @@ export const CardHeader = ({
         <StarIcon className="size-9 text-emerald-300" />
         <h3 className="text-3xl font-serif">{title}</h3>
       </div>
-      <p className="text-sm text-white/60 mt-2 md:mt-3">{description}</p>
+      <p className="text-sm text-white/60 mt-2 md:mt-3 max-h-36 overflow-y-scroll pr-1 custom-scroll-bar">
+        {description}
+      </p>
     </div>
   );
 };
